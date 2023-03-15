14aa16cfa653a045bb122222950802f0f55cc4ac
misspia <pia_leung@hotmail.com>
2023-03-15 21:12:35 +0000

    import sketch

---
+++ src/audio/meta.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://raw.githubusercontent.com/misspia/assets/master/audio/';
+
+export const audioMeta = {
+  url: `${BASE_URL}max-wrong.mp3`,
+  title: `Wrong (ft. Lil Uzi Vert) - MAX`,
+}
